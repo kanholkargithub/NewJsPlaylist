99e30f79405f3c7ed3d25ef1561fbc342d6d62e9
Anushri Kanholkar <117392315+kanholkargithub@users.noreply.github.com>
2024-08-14 18:47:46 +0000

    Commit

---
+++ 01Basics/04Numbers.js
@@ -0,0 +1,28 @@
+// const score =400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+// const OtherNum = 123.34567;
+// console.log(OtherNum.toPrecision(5));
+
+// const hundereds = 100000000
+// console.log(hundereds.toLocaleString());
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++Maths+++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4.2,5,3,4.1));
+
+console.log(Math.random());
